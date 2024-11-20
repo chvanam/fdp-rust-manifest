0e813ad350c415d8eafe4e7bdb71ea29863cff0b
Charles van Amersfoort <ch.vanam@outlook.com>
2024-11-20 01:14:59 +0100

    bench

--- benchmarks/bench.sh
+++ benchmarks/bench.sh
@@ -21,7 +21,7 @@ asyncapi_python() {
     asyncapi generate fromTemplate ../examples/asyncapi/1_message.yaml @asyncapi/python-paho-template -o temp/python --force-write
 }
 
-rust_manifest_doc(){
+rust_doc(){
     cargo run --manifest-path ../fdp-core/Cargo.toml --package fdp-definition --release --bin graph
 }
 
@@ -30,19 +30,19 @@ rust_python(){
 }
 
 # Export functions
-export -f asyncapi_pydantic
+# export -f asyncapi_pydantic
 export -f asyncapi_doc
 export -f asyncapi_python
-export -f rust_manifest_doc
+export -f rust_doc
 export -f rust_python
 
 # Run benchmarks
 echo "Running benchmarks"
-hyperfine --shell=bash --warmup 3 --runs 10 \
-    'rust_manifest_doc' \
+hyperfine --shell=bash --warmup 2 --runs 8 \
     'rust_python' \
-    'asyncapi_doc' \
     'asyncapi_python' \
+    'rust_doc' \
+    'asyncapi_doc' \
     --export-json $OUTPUT_FILE \
 
 echo "Benchmarks completed"
